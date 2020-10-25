f399b3ce51c98722c830c2a3e92546eff62d9272
VIHAN-PC <vihankarnik@gmail.com>
2020-10-26 00:00:07 +0530

    removed the need to install nircmd

--- MuteSpotify.bat
+++ MuteSpotify.bat
@@ -1,8 +1,8 @@
 @echo off
 :while
 for /F "tokens=1,2" %%i in ('tasklist /FI "WINDOWTITLE eq Advertisement" /fo table /nh') do set pid1=%%j
-if not "%pid1%"=="No" (nircmd muteappvolume spotify.exe 1) else (for /F "tokens=1,2" %%i in ('tasklist /FI "WINDOWTITLE eq Spotify" /fo table /nh') do set pid2=%%j
-if not "%pid2%"=="No" (nircmd muteappvolume spotify.exe 1) else (nircmd muteappvolume spotify.exe 0))
+if not "%pid1%"=="No" (nircmd.exe muteappvolume spotify.exe 1) else (for /F "tokens=1,2" %%i in ('tasklist /FI "WINDOWTITLE eq Spotify" /fo table /nh') do set pid2=%%j
+if not "%pid2%"=="No" (nircmd.exe muteappvolume spotify.exe 1) else (nircmd.exe muteappvolume spotify.exe 0))
 timeout 2 /nobreak >nul
 tasklist | find /i "spotify.exe" >nul || exit /B
 goto :while
