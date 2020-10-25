7f2d7b16eebc4c4fd77f2271f32eaefbe2758b0a
VIHAN-PC <vihankarnik@gmail.com>
2020-10-25 23:57:59 +0530

    done the final edit

--- MuteSpotify.bat
+++ MuteSpotify.bat
@@ -3,7 +3,6 @@
 for /F "tokens=1,2" %%i in ('tasklist /FI "WINDOWTITLE eq Advertisement" /fo table /nh') do set pid1=%%j
 if not "%pid1%"=="No" (nircmd muteappvolume spotify.exe 1) else (for /F "tokens=1,2" %%i in ('tasklist /FI "WINDOWTITLE eq Spotify" /fo table /nh') do set pid2=%%j
 if not "%pid2%"=="No" (nircmd muteappvolume spotify.exe 1) else (nircmd muteappvolume spotify.exe 0))
-echo %pid1% & echo %pid2%
 timeout 2 /nobreak >nul
 tasklist | find /i "spotify.exe" >nul || exit /B
 goto :while
